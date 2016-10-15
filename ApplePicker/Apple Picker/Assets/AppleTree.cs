367b6566a41700df35f3826dcf9d48ecfdaa0c5f
jginnard <jeremyginnard@gmail.com>
2016-10-15 13:50:27 -0400

    Started Project Apple Picker

---
+++ ApplePicker/Apple Picker/Assets/AppleTree.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTree : MonoBehaviour {
+
+    //Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //Speed at which the AppleTree moves in meters/second
+    public float speed = 1f;
+
+    //Chance that the AppleTree will change directions
+    public float leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change directions
+    public float chanceToChangeDirections - 0.1f;
+
+    // Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+	// Use this for initialization
+	void Start () {
+	    //Dropping apples every second
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // Basic Movement
+        // Changing Direction
+	}
+}
